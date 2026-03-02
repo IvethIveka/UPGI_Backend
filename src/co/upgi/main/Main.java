9af2a65cace09ba4b08fa539860a622bf00956e7
Iveth Rivera <ivethrivera85@gmail.com>
2026-03-01 21:39:29 -0500

    actualizacion

--- src/co/upgi/main/Main.java
+++ src/co/upgi/main/Main.java
@@ -10,13 +10,45 @@ public class Main {
 
         ReservaDAO dao = new ReservaDAO();
 
-        // Insertar una nueva reserva
-        Reserva r = new Reserva("Iveth", Date.valueOf("2026-03-10"), "18:00", 1);
-        dao.insertar(r);
+        // 1. Insertar dos reservas
+        Reserva r1 = new Reserva("Iveth", Date.valueOf("2026-03-10"), "18:00", 1);
+        dao.insertar(r1);
 
-        // Consultar todas las reservas y mostrarlas en consola
+        Reserva r2 = new Reserva("Nohely", Date.valueOf("2026-03-11"), "20:00", 2);
+        dao.insertar(r2);
+
+        // 2. Consultar todas las reservas y mostrarlas en consola
+        System.out.println("---- CONSULTA INICIAL ----");
         dao.consultar().forEach(res ->
-                System.out.println(res.getNombreCliente())
+                System.out.println(res.getId() + " - " + res.getNombreCliente() + " - " + res.getHora())
         );
+
+        // 3. Actualizar reservas
+        r1.setNombreCliente("Iveth Actualizada");
+        r1.setHora("19:00");
+        dao.actualizar(r1);
+
+        r2.setNombreCliente("Nohely Actualizada");
+        r2.setHora("21:00");
+        dao.actualizar(r2);
+
+        System.out.println("---- CONSULTA DESPUÉS DE ACTUALIZAR ----");
+        dao.consultar().forEach(res ->
+                System.out.println(res.getId() + " - " + res.getNombreCliente() + " - " + res.getHora())
+        );
+
+        // 4. Eliminar solo la reserva de Nohely
+        dao.eliminar(r2.getId());
+
+        System.out.println("---- CONSULTA FINAL DESPUÉS DE ELIMINAR ----");
+        dao.consultar().forEach(res ->
+                System.out.println(res.getId() + " - " + res.getNombreCliente() + " - " + res.getHora())
+        );
+        dao.consultar().forEach(res -> {
+            if(res.getNombreCliente().equals("Nohely")) {
+                dao.eliminar(res.getId());
+            }
+        });
+
     }
 }
